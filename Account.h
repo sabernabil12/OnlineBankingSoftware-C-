6c86d40063a69faae52da849d217e72c4e7f7f58
sabernabil12 <SABERNABIL12@GMAIL.COM>
2017-08-01 00:22:27 -0700

    Create Account.h

---
+++ Account.h
@@ -0,0 +1,29 @@
+#ifndef acnt
+
+#define acnt
+
+class account
+{
+public:
+	account();
+	account(double b, int n);
+
+	int getAccountNo();
+	void setAccountNo(int n);
+	void read();
+	void deposit(double d);
+	void withdraw(double w);
+	void transfer(double amount);
+	void transferAdd(int x, double income);
+	void print() const;
+	void printMainMenu() const;
+	void printTransactionMenu() const;
+private:
+	double balance;
+	int number;
+	double d;
+	double w;
+	double amount;
+};
+
+#endif
